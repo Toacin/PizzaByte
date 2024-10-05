d4be86be53dc116a7f95513ba08f80da93bf2d09
Toacin <toacinp@outlook.com>
2024-10-04 18:51:01 -0700

    server side updated with test route and new logging mechanism

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const log = require("./logger");
 
 function startServer() {
   const app = express();
@@ -11,9 +12,12 @@ function startServer() {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cors());
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
 
   const httpServer = app.listen(port, () => {
-    console.log("Backend Server listening on port " + port + ".");
+    log.info("Backend Server listening on port " + port + ".");
   });
 
   return { app, httpServer };
