79a44377eec69191d622ed68f782c8219e67c74e
Toacin <toacinp@outlook.com>
2024-10-05 19:57:17 -0700

    classics and toppings routes generated

---
+++ backend/APIroutes/toppingsRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const controller = require("../APIcontrollers/toppingsController");
+const authenticateToken = require("../middlewares/authenticateUserToken");
+
+// ==================================================
+// current path: /api/toppings
+// ==================================================
+
+router.get("/", controller.getToppings);
+router.post("/", authenticateToken, controller.addToppings);
+router.delete("/:toppingId", authenticateToken, controller.deleteTopping);
+
+module.exports = router;
