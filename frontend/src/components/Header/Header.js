165e0fba2cd9b801a396c257a090a1857df53d47
Toacin <toacinp@outlook.com>
2024-10-05 20:54:30 -0700

    header file generated

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import pizzaByteSmallWhite from "../../assets/PizzaByteSmallWhite.png";
+
+export default function Header() {
+  return (
+    <div className="fixed bg-black py-6 px-10 md:px-24 min-w-full">
+      <div className="flex justify-between items-center">
+        <img
+          className="w-[40%] md:w-[25%] lg:w-[15%] xl:w-[10%]"
+          src={pizzaByteSmallWhite}
+          alt="PizzaByte Logo"
+        />
+        <div>
+          <button className="bg-white rounded p-2">Sign In</button>
+        </div>
+      </div>
+    </div>
+  );
+}
