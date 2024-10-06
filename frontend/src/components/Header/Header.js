6a975416093184f45bcb24ca7ae919a6fe436b50
Toacin <toacinp@outlook.com>
2024-10-05 22:41:56 -0700

    header modified z index

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -2,7 +2,7 @@ import pizzaByteSmallWhite from "../../assets/PizzaByteSmallWhite.png";
 
 export default function Header() {
   return (
-    <div className="fixed bg-black py-6 px-10 md:px-24 min-w-full">
+    <div className="fixed bg-black py-6 px-10 md:px-24 min-w-full z-10">
       <div className="flex justify-between items-center">
         <img
           className="w-[40%] md:w-[25%] lg:w-[15%] xl:w-[10%]"
