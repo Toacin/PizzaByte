b2dde6a7f47014af6b55fef50f8e1b2cf13db513
Toacin <toacinp@outlook.com>
2024-10-05 18:36:38 -0700

    authenticate middleware function refined

--- backend/middlewares/authenticateUserToken.js
+++ backend/middlewares/authenticateUserToken.js
@@ -1,36 +1,22 @@
 const jwt = require("jsonwebtoken");
-const log = require("../log");
+const log = require("../logger");
 
 const secret = process.env.JWT_SECRET;
 
 // tokens will expire in 90000 seconds (25 hours)
 const expiration = 90000;
 
-// create a set of paths that do not need authentication
-const unauthenticatedPaths = new Set([
-  "/api/auth/login",
-  "/api/auth/signup",
-  "/api/testRoute",
-]);
-
-// check if the path is in the set of unauthenticated paths
-const shouldSkipAuth = (req) => {
-  return unauthenticatedPaths.has(req.path);
-};
-
 const authenticateToken = (req, res, next) => {
-  if (shouldSkipAuth(req)) {
-    return next();
-  }
-
   // pull the token from the request headers and remove the Bearer prefix
-  let token = req.headers.authorization.split(" ").pop().trim();
+  let token = req.headers.authorization;
 
   // if the token is a falsy value return an error
   if (!token) {
     log.info("Authentication failed - no token provided");
     return res.sendStatus(401);
   }
+
+  token = token.split(" ").pop().trim();
 
   // we have a token so now we try to verify it
   try {
