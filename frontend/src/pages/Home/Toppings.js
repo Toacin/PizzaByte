910e4a30843a543abd7e2f27c5d3689762165d45
Toacin <toacinp@outlook.com>
2024-10-07 22:30:50 -0700

    topping file organized and fetch logic moved

--- frontend/src/pages/Home/Toppings.js
+++ frontend/src/pages/Home/Toppings.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import auth from "../../utils/auth";
 import { useGlobalState } from "../../GlobalStateProvider";
 import toast from "react-hot-toast";
+import ToppingsNetworkServices from "./ToppingsNetworkServices";
+import scrollToSection from "../../utils/scrollToSection";
 
 export default function Toppings() {
   const [toppings, setToppings] = useState([]);
@@ -12,130 +14,70 @@ export default function Toppings() {
   const [editedTopping, setEditedTopping] = useState("");
   const [updatedTopping, setUpdatedTopping] = useState("");
 
+  // Fetch existing toppings
   useEffect(() => {
     if (auth.loggedIn()) {
       setRole(auth.getProfile().data.role);
     }
     (async function () {
-      try {
-        const response = await fetch("/api/toppings", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        if (!response.ok) {
-          console.error("Request Failed");
-          if (response.status !== 500) {
-            const errorData = await response.json();
-            toast.error(errorData.message);
-          } else {
-            toast.error("Something went wrong. Please try again later.");
-          }
-          return;
-        }
-        const data = await response.json();
-        const allToppings = data.toppings.map((topping) => topping.toppingName);
-        setToppings(allToppings);
-        dispatch({ type: "SET_TOPPINGS", payload: allToppings });
-      } catch (err) {
-        console.error(err.toString());
-        toast.error("Something went wrong. Please try again later.");
-      }
+      await ToppingsNetworkServices.getAllToppings(
+        toast,
+        setToppings,
+        dispatch,
+      );
     })();
   }, [toppingModified]);
 
-  const deleteTopping = async (topping) => {
-    try {
-      const response = await fetch(`/api/toppings/${topping}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          authorization: `Bearer ${auth.getToken()}`,
-        },
-      });
-      if (!response.ok) {
-        console.error("Request Failed");
-        if (response.status !== 500) {
-          const errorData = await response.json();
-          toast.error(errorData.message);
-        } else {
-          toast.error("Something went wrong. Please try again later.");
-        }
-        return;
-      }
-      setToppingModified(toppingModified + 1);
-    } catch (err) {
-      console.error(err.toString());
-      toast.error("Something went wrong. Please try again later.");
+  // scroll to the update form when a classic is edited
+  useEffect(() => {
+    if (editedTopping) {
+      scrollToSection("update-topping");
     }
+  }, [editedTopping]);
+
+  // add topping
+  const addTopping = async (e) => {
+    await ToppingsNetworkServices.addToppings(
+      e,
+      auth,
+      newTopping,
+      toast,
+      setToppingModified,
+      toppingModified,
+      setNewTopping,
+    );
   };
 
-  const handleAddTopping = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("/api/toppings", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          authorization: `Bearer ${auth.getToken()}`,
-        },
-        body: JSON.stringify({ toppingName: newTopping }),
-      });
-      if (!response.ok) {
-        console.error("Failed to add topping");
-        if (response.status !== 500) {
-          const errorData = await response.json();
-          toast.error(errorData.message);
-        } else {
-          toast.error("Something went wrong. Please try again later.");
-        }
-        return;
-      }
-      setToppingModified(toppingModified + 1);
-      setNewTopping("");
-    } catch (err) {
-      console.error(err.toString());
-      toast.error("Something went wrong. Please try again later.");
-    }
-  };
-
+  // initiate topping update
   const initiateToppingUpdate = async (originalTopping) => {
     setEditedTopping(originalTopping);
     setUpdatedTopping(originalTopping);
   };
 
+  // update topping
   const updateTopping = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("/api/toppings", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          authorization: `Bearer ${auth.getToken()}`,
-        },
-        body: JSON.stringify({
-          toppingName: updatedTopping,
-          originalToppingName: editedTopping,
-        }),
-      });
-      if (!response.ok) {
-        console.error("Failed to update topping");
-        if (response.status !== 500) {
-          const errorData = await response.json();
-          toast.error(errorData.message);
-        } else {
-          toast.error("Something went wrong. Please try again later.");
-        }
-        return;
-      }
-      setToppingModified(toppingModified + 1);
-      setUpdatedTopping("");
-      setEditedTopping("");
-    } catch (err) {
-      console.error(err.toString());
-      toast.error("Something went wrong. Please try again later.");
-    }
+    await ToppingsNetworkServices.updateToppings(
+      e,
+      auth,
+      updatedTopping,
+      editedTopping,
+      toast,
+      setToppingModified,
+      toppingModified,
+      setUpdatedTopping,
+      setEditedTopping,
+    );
+  };
+
+  // delete topping
+  const deleteTopping = async (topping) => {
+    await ToppingsNetworkServices.deleteToppings(
+      topping,
+      auth,
+      toast,
+      setToppingModified,
+      toppingModified,
+    );
   };
 
   return (
@@ -145,117 +87,163 @@ export default function Toppings() {
         Choose your favorite toppings. Made with real and fresh ingredients
       </h2>
       <div className="min-w-[100%] md:min-w-[80%] lg:min-w-[70%] xl:min-w-[60%] border rounded bg-zinc-100 mt-4 p-5 lg:p-20">
-        {toppings.map((topping) => (
-          <div key={topping} className="flex items-center justify-between mt-2">
-            <p className="text-xl md:text-3xl">
-              {topping[0].toUpperCase() + topping.substring(1)}
-            </p>
-            <div>
-              <button
-                className={`${
-                  role === "owner"
-                    ? "bg-black cursor-pointer text-white"
-                    : "bg-gray-500 cursor-not-allowed"
-                } text-white rounded p-2 mr-2`}
-                disabled={role !== "owner"}
-                onClick={() => initiateToppingUpdate(topping)}
-              >
-                Edit
-              </button>
-              <button
-                className={`${
-                  role === "owner"
-                    ? "bg-red-500 cursor-pointer"
-                    : "bg-gray-500 cursor-not-allowed"
-                } text-white rounded p-2`}
-                disabled={role !== "owner"}
-                onClick={() => deleteTopping(topping)}
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        ))}
+        <MapExisingToppings
+          {...{ toppings, role, initiateToppingUpdate, deleteTopping }}
+        />
 
-        <form
-          onSubmit={handleAddTopping}
-          className="min-w-full flex justify-between items-center mt-6"
+        <AddToppingForm {...{ addTopping, newTopping, setNewTopping, role }} />
+
+        <UpdateToppingForm
+          {...{
+            editedTopping,
+            updateTopping,
+            updatedTopping,
+            setUpdatedTopping,
+            role,
+            setEditedTopping,
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function MapExisingToppings({
+  toppings,
+  role,
+  initiateToppingUpdate,
+  deleteTopping,
+}) {
+  return (
+    <>
+      {toppings.map((topping) => (
+        <div
+          key={topping}
+          className="flex items-center justify-between mt-2 border-b border-gray-300 pb-3"
         >
-          <input
-            className="border p-2 rounded mt-4 min-w-[80%]"
-            type="text"
-            name="topping"
-            value={newTopping}
-            onChange={(e) => setNewTopping(e.target.value)}
-            placeholder={
-              role === "owner"
-                ? "Enter a new topping"
-                : "Sign in as an owner to add a new topping"
-            }
-            required
-            disabled={role !== "owner"}
-          />
-          <button
-            type="submit"
-            className={`${
-              role === "owner"
-                ? "bg-black cursor-pointer"
-                : "bg-gray-500 cursor-not-allowed"
-            } text-white p-2 rounded mt-4`}
-            disabled={role !== "owner"}
-            onClick={handleAddTopping}
-          >
-            Add
-          </button>
-        </form>
-
-        {editedTopping && (
+          <p className="text-xl md:text-3xl">
+            {topping[0].toUpperCase() + topping.substring(1)}
+          </p>
           <div>
-            <form
-              onSubmit={updateTopping}
-              className="min-w-full flex justify-between items-center mt-6"
+            <button
+              className={`${
+                role === "owner"
+                  ? "bg-black cursor-pointer text-white"
+                  : "bg-gray-500 cursor-not-allowed"
+              } text-white rounded p-2 mr-2`}
+              disabled={role !== "owner"}
+              onClick={() => initiateToppingUpdate(topping)}
             >
-              <input
-                className="border p-2 rounded mt-4 min-w-[80%]"
-                type="text"
-                name="updatedTopping"
-                value={updatedTopping}
-                onChange={(e) => setUpdatedTopping(e.target.value)}
-                placeholder={
-                  role === "owner"
-                    ? "Update Topping Name"
-                    : "Sign in as an owner to add a new topping"
-                }
-                required
-                disabled={role !== "owner"}
-              />
-              <button
-                type="submit"
-                className={`${
-                  role === "owner"
-                    ? "bg-black cursor-pointer"
-                    : "bg-gray-500 cursor-not-allowed"
-                } text-white p-2 rounded mt-4`}
-                disabled={role !== "owner"}
-                onClick={updateTopping}
-              >
-                Update
-              </button>
-            </form>
+              Edit
+            </button>
             <button
               className={`${
                 role === "owner"
                   ? "bg-red-500 cursor-pointer"
                   : "bg-gray-500 cursor-not-allowed"
-              } text-white p-2 rounded mt-4 min-w-full`}
+              } text-white rounded p-2`}
               disabled={role !== "owner"}
-              onClick={() => setEditedTopping("")}
+              onClick={() => deleteTopping(topping)}
             >
-              Cancel Update
+              Delete
             </button>
           </div>
-        )}
-      </div>
-    </div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+function AddToppingForm({ addTopping, newTopping, setNewTopping, role }) {
+  return (
+    <form
+      onSubmit={addTopping}
+      className="min-w-full flex justify-between items-center mt-6"
+    >
+      <input
+        className="border p-2 rounded mt-4 min-w-[80%]"
+        type="text"
+        name="topping"
+        value={newTopping}
+        onChange={(e) => setNewTopping(e.target.value)}
+        placeholder={
+          role === "owner"
+            ? "Enter a new topping"
+            : "Sign in as an owner to add a new topping"
+        }
+        required
+        disabled={role !== "owner"}
+      />
+      <button
+        type="submit"
+        className={`${
+          role === "owner"
+            ? "bg-black cursor-pointer"
+            : "bg-gray-500 cursor-not-allowed"
+        } text-white p-2 rounded mt-4`}
+        disabled={role !== "owner"}
+      >
+        Add
+      </button>
+    </form>
+  );
+}
+
+function UpdateToppingForm({
+  editedTopping,
+  updateTopping,
+  updatedTopping,
+  setUpdatedTopping,
+  role,
+  setEditedTopping,
+}) {
+  return (
+    <>
+      {editedTopping && (
+        <div className="border-t border-gray-300 mt-6" id="update-topping">
+          <form
+            onSubmit={updateTopping}
+            className="min-w-full flex justify-between items-center"
+          >
+            <input
+              className="border p-2 rounded mt-4 min-w-[80%]"
+              type="text"
+              name="updatedTopping"
+              value={updatedTopping}
+              onChange={(e) => setUpdatedTopping(e.target.value)}
+              placeholder={
+                role === "owner"
+                  ? "Update Topping Name"
+                  : "Sign in as an owner to add a new topping"
+              }
+              required
+              disabled={role !== "owner"}
+            />
+            <button
+              type="submit"
+              className={`${
+                role === "owner"
+                  ? "bg-black cursor-pointer"
+                  : "bg-gray-500 cursor-not-allowed"
+              } text-white p-2 rounded mt-4`}
+              disabled={role !== "owner"}
+            >
+              Update
+            </button>
+          </form>
+          <button
+            className={`${
+              role === "owner"
+                ? "bg-red-500 cursor-pointer"
+                : "bg-gray-500 cursor-not-allowed"
+            } text-white p-2 rounded mt-4 min-w-full`}
+            disabled={role !== "owner"}
+            onClick={() => setEditedTopping("")}
+          >
+            Cancel Update
+          </button>
+        </div>
+      )}
+    </>
   );
 }
